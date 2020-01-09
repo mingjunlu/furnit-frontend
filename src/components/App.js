c80bc7ff90cf41902135cbe720fffff15a50d4a3
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-01-09 10:13:22 +0800

    Add Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './shared/Header/Header';
 import Home from './Home/Home';
+import Footer from './shared/Footer/Footer';
 
 const App = () => (
     <>
         <Header />
         <Home />
+        <Footer />
     </>
 );
 
---
+++ src/components/shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => (
+    <section className={styles.section}>
+        <footer className={styles.footer}>
+            <p>© 2020 Furnit. All rights reserved.</p>
+        </footer>
+    </section>
+);
+
+export default Footer;
