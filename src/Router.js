ed28486c6f141638df1d2f00446c840e3b623582
mingjun <40516784+mingjunlu@users.noreply.github.com>
2020-01-21 19:57:34 +0800

    Scroll to top when the route changes

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomePage from './pages/HomePage/HomePage';
@@ -8,6 +9,7 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 const Router = () => (
     <BrowserRouter>
+        <ScrollToTop />
         <Header />
         <Switch>
             <Route exact path="/">
